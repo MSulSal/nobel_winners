0ba6b9f58d50e460d73199dea57e290ef584c2d7
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-05-04 22:34:51 -0500

    first crawl

--- nobel_winners/spiders/nwinners_list_spider.py
+++ nobel_winners/spiders/nwinners_list_spider.py
@@ -1,10 +1,29 @@
 import scrapy
 
+# define the data to be scraped
+class NWinnerItem(scrapy.Item):
+    country = scrapy.Field()
+    name = scrapy.Field()
+    link_text = scrapy.Field()
 
-class NwinnersListSpiderSpider(scrapy.Spider):
-    name = "nwinners_list_spider"
+
+# create a named spider
+class NWinnerSpider(scrapy.Spider):
+    """ Scrapes the country and link text for Nobel winners. """
+
+    name = "nwinners_list"
     allowed_domains = ["en.wikipedia.org"]
     start_urls = ["https://en.wikipedia.org/wiki/List_of_Nobel_laureates_by_country"]
 
     def parse(self, response):
-        pass
+        h3s = response.xpath("//h3")
+
+        for h3 in h3s:
+            country = h3.xpath("text()").extract()
+            if country:
+                winners = h3.xpath("../following-sibling::ol[1]")
+                for w in winners.xpath("li"):
+                    text = w.xpath("descendant-or-self::text()").extract()
+                    link = ''.join(text)
+                    yield NWinnerItem(country=country[0], name=text[0], link_text=link)
+
