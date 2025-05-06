64d9f0802891e78955f8384fc4b59be4d33c5efb
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-05-05 23:32:32 -0500

    use helper method for processing winner data

--- nobel_winners/spiders/nwinners_list_spider.py
+++ nobel_winners/spiders/nwinners_list_spider.py
@@ -1,10 +1,63 @@
 import scrapy
+import re
+
+BASE_URL = "http://en.wikipedia.org"
+
+def process_winner_li(w, country=None):
+    """
+    Process a winner's <li> tag, adding country of birth or nationality,
+    as applicable.
+    """
+    wdata = {}
+    wdata["link"] = BASE_URL + w.xpath("a/@href").extract()[0]
+
+    text = "".join(w.xpath("descendant-or-self::text()").extract())
+    wdata["name"] = text.split(",")[0].strip()
+
+    year = re.findall("\d{4}", text)
+
+    if year:
+        wdata["year"] = int(year[0])
+    else:
+        wdata["year"] = 0
+        print("Oops, no year in", text)
+
+    category = re.findall(
+        "Physics|Chemistry|Physiology or Medicine|Literature|Peace|Economics",
+        text
+    )
+
+    if category:
+        wdata["category"] = category[0]
+    else:
+        wdata["category"] = ""
+        print("Oops, no category in", text)
+
+    if country:
+        if text.find("*") != -1:
+            wdata["country"] = ""
+            wdata["born_in"] = country
+        else:
+            wdata["country"] = country
+            wdata["born_in"] = ""
+
+    wdata["text"] = text
+    return wdata
 
 # define the data to be scraped
 class NWinnerItem(scrapy.Item):
-    country = scrapy.Field()
     name = scrapy.Field()
-    link_text = scrapy.Field()
+    link = scrapy.Field()
+    year = scrapy.Field()
+    category = scrapy.Field()
+    country = scrapy.Field()
+    # gender = scrapy.Field()
+    # born_in = scrapy.Field()
+    # date_of_birth = scrapy.Field()
+    # data_of_death = scrapy.Field()
+    # place_of_birth = scrapy.Field()
+    # place_of_death = scrapy.Field()
+    # text = scrapy.Field()
 
 
 # create a named spider
@@ -19,11 +72,16 @@ class NWinnerSpider(scrapy.Spider):
         h3s = response.xpath("//h3")
 
         for h3 in h3s:
-            country = h3.xpath("text()").extract()
+            country = h3.xpath("text()").extract()[0]
             if country:
                 winners = h3.xpath("../following-sibling::ol[1]")
                 for w in winners.xpath("li"):
-                    text = w.xpath("descendant-or-self::text()").extract()
-                    link = ''.join(text)
-                    yield NWinnerItem(country=country[0], name=text[0], link_text=link)
+                    wdata = process_winner_li(w, country)
+                    yield NWinnerItem(
+                        name=wdata["name"],
+                        link=wdata["link"],
+                        year=wdata["year"],
+                        country=wdata["country"],
+                        category=wdata["category"]
+                    )
 
